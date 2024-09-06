af6eefd201e5d8135e8e383bb0d4eb4357e31437
adslasalaMA <markpetpet0827@gmail.com>
2024-09-05 23:45:58 -0700

    initial commit for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,35 @@
 <?php
-
+use app\Models\Student;
 use Illuminate\Support\Facades\Route;
+
+Route::get('Students/create', function(){
+    $Student = new Student();
+    $Student->first_name = 'John';
+    $Student->last_name = 'Doe';
+    $Student->email = 'johndoe@example.com';
+    $Student->age = 22;
+    $Student->save();
+    return 'Student Created!';
+});
+
+route::get('/Students', function() {
+    $Students = Student::all();
+    return $Students;
+});
+
+Route::get('/Srudents/update', function() {
+    $Student = Student::where('email', 'johndoe@example.com')->first();
+    $Student->email = 'john.doe@example.com';
+    $Student->age = 23;
+    $Student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/Srudents/delete', function() {
+    $Student = Student::where('email', 'johndoe@example.com')->first();
+    $Student->delete();
+    return 'Student Deleted!';
+});
 
 /*
 |--------------------------------------------------------------------------
